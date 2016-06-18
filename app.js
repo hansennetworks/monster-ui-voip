a9a8edd0ce8a452c5e0c87f836cfff90d1464d9a
adamrogas <adam@rogassi.com>
2016-06-17 21:49:58 -0700

    PBX Updates

--- app.js
+++ app.js
@@ -1,90 +1,90 @@
-define(function(require){
-	var $ = require('jquery'),
+define(function (require) {
+    var $ = require('jquery'),
 		_ = require('underscore'),
 		monster = require('monster');
 
-	var app = {
-		name: 'voip',
+    var app = {
+        name: 'voip',
 
-		css: [ 'app' ],
+        css: ['app'],
 
-		i18n: { 
-			'en-US': { customCss: false },
-			'fr-FR': { customCss: false },
-			'ru-RU': { customCss: false },
-			'es-ES': { customCss: false }
-		},
+        i18n: {
+            'en-US': { customCss: false },
+            'fr-FR': { customCss: false },
+            'ru-RU': { customCss: false },
+            'es-ES': { customCss: false }
+        },
 
-		requests: {},
-		subscribe: {},
+        requests: {},
+        subscribe: {},
 
-		subModules: ['devices', 'groups', 'numbers', 'strategy', 'callLogs', 'users', 'myOffice', 'featureCodes', 'vmboxes'],
+        subModules: ['devices', 'groups', 'numbers', 'strategy', 'callLogs', 'users', 'licenses', 'myOffice', 'featureCodes', 'vmboxes'],
 
-		load: function(callback){
-			var self = this;
+        load: function (callback) {
+            var self = this;
 
-			self.initApp(function() {
-				callback && callback(self);
-			});
-		},
+            self.initApp(function () {
+                callback && callback(self);
+            });
+        },
 
-		initApp: function(callback) {
-			var self = this;
+        initApp: function (callback) {
+            var self = this;
 
-			monster.pub('auth.initApp', {
-				app: self,
-				callback: callback
-			});
-		},
+            monster.pub('auth.initApp', {
+                app: self,
+                callback: callback
+            });
+        },
 
-		render: function(container){
-			var self = this,
+        render: function (container) {
+            var self = this,
 				parent = container || $('#monster-content'),
 				template = $(monster.template(self, 'app'));
 
-			/* On first Load, load my office */
-			template.find('.category#myOffice').addClass('active');
-			monster.pub('voip.myOffice.render', { parent: template.find('.right-content') });
+            /* On first Load, load my office */
+            template.find('.category#myOffice').addClass('active');
+            monster.pub('voip.myOffice.render', { parent: template.find('.right-content') });
 
-			self.bindEvents(template);
+            self.bindEvents(template);
 
-			parent
+            parent
 				.empty()
 				.append(template);
-		},
+        },
 
-		formatData: function(data) {
-			var self = this;
-		},
+        formatData: function (data) {
+            var self = this;
+        },
 
-		bindEvents: function(parent) {
-			var self = this,
+        bindEvents: function (parent) {
+            var self = this,
 				container = parent.find('.right-content');
 
-			parent.find('.left-menu').on('click', '.category:not(.loading)', function() {
-				// Get the ID of the submodule to render
-				var $this = $(this),
+            parent.find('.left-menu').on('click', '.category:not(.loading)', function () {
+                // Get the ID of the submodule to render
+                var $this = $(this),
 					args = {
-						parent: container,
-						callback: function() {
-							parent.find('.category').removeClass('loading');
-						}
+					    parent: container,
+					    callback: function () {
+					        parent.find('.category').removeClass('loading');
+					    }
 					},
 					id = $this.attr('id');
 
-				// Display the category we clicked as active
-				parent
+                // Display the category we clicked as active
+                parent
 					.find('.category')
 					.removeClass('active')
 					.addClass('loading');
-				$this.toggleClass('active');
+                $this.toggleClass('active');
 
-				// Empty the main container and then render the submodule content
-				container.empty();
-				monster.pub('voip.' + id + '.render', args);
-			});
-		}
-	};
+                // Empty the main container and then render the submodule content
+                container.empty();
+                monster.pub('voip.' + id + '.render', args);
+            });
+        }
+    };
 
-	return app;
+    return app;
 });
